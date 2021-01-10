5ad5432a095bf52b9b2667c2a49eb84f4602c02e
nitheesh <nitheeshhs@gmail.com>
2021-01-10 10:35:07 +0530

    commit

---
+++ BlazorMovieApp/Shared/Entities/Movie.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorMovieApp.Shared.Entities
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
